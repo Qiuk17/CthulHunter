3ae65c792a4b55848193fc18f44e4c55072d87e1
tube <gan868686@qq.com>
2019-07-16 17:30:36 +0800

    创建了Character类，给Object对象增加了Id属性

---
+++ CthulHunter/ChCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CthulHunter
+{
+    public class ChCharacter:ChCreature
+    {
+        public string Name { get; set; }
+        public int MaxHp { get => (STR + CON) / 10; }
+        public int Hp { get; set; }
+        public int MOV { get; set; }
+        public ChCharacter()
+        {
+        }
+    }
+}
--- CthulHunter/ChObject.cs
+++ CthulHunter/ChObject.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Timers;
 namespace CthulHunter
 {
     public class ChObject
     {
+        public int Id { get => GetHashCode(); }
         public ChObject()
         {
         }
