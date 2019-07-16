22915460296213ff7c5b1bf9479f90ab29e07f5d
tube <gan868686@qq.com>
2019-07-16 17:16:32 +0800

    添加生物类（基类） 包含九大基本属性

---
+++ CthulHunter/ChCreature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CthulHunter
+{
+    public class ChCreature : ChObject
+    {
+        public int STR { get; set; }
+        public int CON { get; set; }
+        public int SIZ { get; set; }
+        public int DEX { get; set; }
+        public int APP { get; set; }
+        public int INT { get; set; }
+        public int POW { get; set; }
+        public int EDU { get; set; }
+        public int LUK { get; set; }
+
+        public bool IsDead { get; set; }
+        public ChCreature()
+        {
+            IsDead = false;
+        }
+    }
+}
